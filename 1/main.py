57d68f65327ae3700dc7e3aec28f2f6584855b7a
Linus Jansson <contact@limpan.dev>
2023-12-01 20:17:12 +0100

    if name == main

--- 1/main.py
+++ 1/main.py
@@ -1,30 +1,33 @@
+def main(inp):
+    digit_list = []
+    for line in inp:
+        digit_string = ""
+        for char in line:
+            # If char is a digit 
+            if char.isdigit():
+                # If length of tmp is longer or equal than 2,
+                # We replace the last digit with the current digit
+                if len(digit_string) >= 2:
+                    digit_string = digit_string[:-1]
+                    digit_string += char
+                else: 
+                    digit_string += char
+        # If the line only contains one digit we add the same again
+        if len(digit_string) == 1:
+            digit_string = digit_string*2
 
-inp = ""
+        digit_string = int(digit_string)
+        digit_list.append(digit_string)
 
-f = open("input.txt", "r")
-inp = f.readlines()
-f.close()
-
-digit_list = []
-
-for line in inp:
-    digit_string = ""
-    for char in line:
-        # If char is a digit 
-        if char.isdigit():
-            # If length of tmp is longer or equal than 2,
-            # We replace the last digit with the current digit
-            if len(digit_string) >= 2:
-                digit_string = digit_string[:-1]
-                digit_string += char
-            else: 
-                digit_string += char
-    # If the line only contains one digit we add the same again
-    if len(digit_string) == 1:
-        digit_string = digit_string*2
-
-    digit_string = int(digit_string)
-    digit_list.append(digit_string)
+    return digit_list
         
-print(digit_list)
-print(sum(digit_list))
+if __name__ == "__main__":
+    inp = ""
+    f = open("input.txt", "r")
+    inp = f.readlines()
+    f.close()
+
+    digit_list = main(inp)       
+    
+    print(digit_list)
+    print(sum(digit_list))
